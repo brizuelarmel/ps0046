2bc92421a68e6aba04c76328efed25d7c50886db
brizuelarmel <armel.brizuelarodriguez@proton.me>
2026-03-17 16:29:16 -0600

    Fix data error

--- data-raw/DATASETS.R
+++ data-raw/DATASETS.R
@@ -12,17 +12,17 @@ file <- "data-raw/Figures_1_1_and_6_1.xlsx"
 filename <- tools::file_path_sans_ext(basename(file))
 
 # Read the xlsx file
-data <- readxl::read_excel(file)
+Figures_1_1_and_6_1 <- readxl::read_excel(file)
 
 # Add categorical binary variable (Group R: Rich economies, Group B: Not so rich economies)
 group_r_countries <- c("Australia", "Belgium", "Canada", "Croatia", "Denmark", "Finland", "France",
                        "Germany", "Greece", "Hungary", "Iceland", "Ireland", "Israel", "Italy",
                        "Japan", "Netherlands", "Norway", "Poland", "Portugal", "Spain", "Sweden",
                        "Switzerland", "United Kingdom", "United States")
-data$Group <- ifelse(data$Country %in% group_r_countries, "R", "NSR")
+Figures_1_1_and_6_1$Group <- ifelse(Figures_1_1_and_6_1$Country %in% group_r_countries, "R", "NSR")
 
 # Save as dataset
-save(Figures_1_1_and_6_1 = data, file = "data/Figures_1_1_and_6_1.rda", compress = TRUE)
+save(data = Figures_1_1_and_6_1, file = "data/Figures_1_1_and_6_1.rda", compress = TRUE)
 
 # Read the Latinobarometro_2023_Costa_Rica_Spss_esp_v1.sav file
 file_spss <- "data-raw/Latinobarometro_2023_Costa_Rica_Spss_esp_v1.sav"
